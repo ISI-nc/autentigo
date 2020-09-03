97baa12c8bee0a9615fac7d97f0f01416361cdc8
Simon Teles <simon.teles@protonmail.com>
2020-09-03 13:40:09 +0200

    feat(SQL): add testing on update

--- pkg/companion-api/backend/sql/sql_test.go
+++ pkg/companion-api/backend/sql/sql_test.go
@@ -92,6 +92,78 @@ func TestSqlClient_CreateUser(t *testing.T) {
 	}
 }
 
+func TestSqlClient_UpdateUser(t *testing.T) {
+	setup(t)
+	db := DbConnect("postgres",fmt.Sprintf("postgres://postgres:postgres@%s:%s/%s?sslmode=disable", test.PostgresHost(), test.TestPostgresRsource.GetPort("5432/tcp"), "postgres"))
+	err := CreateUsersTableIfNotExists(db,authUsersTableName)
+	if err != nil {
+		t.Fatalf("Error while creating table: %v", err)
+	}
+
+	client := &sqlClient{
+		db: db,
+		table: authUsersTableName,
+	}
+
+	user1Id := "toto"
+	user1Claims := auth.ExtraClaims{
+		DisplayName:   "toto",
+		Email:         "toto@test.net",
+		EmailVerified: false,
+		Groups:        []string{"group1"},
+	}
+	user1 := &backend.UserData{
+		PasswordHash: "hash",
+		ExtraClaims:  user1Claims,
+	}
+
+	err = client.CreateUser(user1Id, user1)
+	if err != nil {
+		t.Fatalf("Error while creating user: %v", err)
+	}
+
+	userFromDb, err := client.GetUser(user1Id)
+	if err != nil {
+		t.Fatalf("Error while getting user: %v", err)
+	}
+
+	if !cmp.Equal(user1, userFromDb) {
+		t.Fatalf("User is different: %s", cmp.Diff(user1, userFromDb))
+	}
+
+	// now we update
+	user1bClaims := auth.ExtraClaims{
+		DisplayName:   "newtoto",
+		Email:         "newtoto@test.net",
+		EmailVerified: true,
+		Groups:        []string{"group1","newgroup"},
+	}
+	user1b := &backend.UserData{
+		PasswordHash: "newhash",
+		ExtraClaims:  user1bClaims,
+	}
+
+	err = client.UpdateUser(user1Id, func(user *backend.UserData) error {
+		user.ExtraClaims = user1bClaims
+		user.PasswordHash = user1b.PasswordHash
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("Error while updating user: %v", err)
+	}
+
+	// we get the updated user
+	updateUserFromDb, err := client.GetUser(user1Id)
+	if err != nil {
+		t.Fatalf("Error while getting user: %v", err)
+	}
+
+	if !cmp.Equal(user1b, updateUserFromDb) {
+		t.Fatalf("Updated user is different: %s", cmp.Diff(user1b, updateUserFromDb))
+	}
+
+}
+
 func TestSqlClient_DeleteUser(t *testing.T) {
 	setup(t)
 	db := DbConnect("postgres",fmt.Sprintf("postgres://postgres:postgres@%s:%s/%s?sslmode=disable", test.PostgresHost(), test.TestPostgresRsource.GetPort("5432/tcp"), "postgres"))
